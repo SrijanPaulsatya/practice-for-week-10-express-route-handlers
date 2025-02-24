74ccf86e34fae0e5c17e150dca9335f082bfb391
Srijan Paulsatya <srijanpaulsatya@gmail.com>
2025-02-24 12:53:52 +0530

    Finish Exercise

--- server/app.js
+++ server/app.js
@@ -34,8 +34,103 @@ app.get('/artists', (req, res) => {
 });
 
 app.post('/artists', (req, res) => {
-  const artist = addArtist({name: req.body['name']}); 
-  res.status(201).json(artist);
+  const newArtist = addArtist({name: req.body['name']}); 
+  res.status(201).json(newArtist);
+});
+
+app.get('/artists/latest', (req, res) => {
+  res.json(getLatestArtist());
+});
+
+app.get('/artists/latest/albums', (req, res) => {
+  res.json(getAlbumsForLatestArtist());
+});
+
+app.get('/artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  res.json(getArtistByArtistId(artistId));
+});
+
+app.put('/artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  const data = req.body;
+  res.json(editArtistByArtistId(artistId, data));
+});
+
+app.delete('/artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  deleteArtistByArtistId(artistId);
+  res.json({message: "Successfully deleted"});
+});
+
+//Albums route
+
+app.get('/artists/:artistId/albums', (req, res) => {
+  const artistId = req.params.artistId;
+  res.json(getAlbumsByArtistId(artistId));
+});
+
+app.get('/albums/:albumId', (req, res) => {
+  const albumId = req.params.albumId;
+  res.json(getAlbumByAlbumId(albumId));
+});
+
+app.post('/artist/:artistId/albums', (req, res) => {
+  const artistId = req.params.artistId;
+  const data = req.body;
+  res.json(addAlbumByArtistId(artistId, data));
+});
+
+app.put('/albums/:albumId', (req, res) => {
+  const albumId = req.params.albumId;
+  const data = req.body;
+  res.json(editAlbumByAlbumId(albumId, data));
+});
+
+app.delete('/albums/:albumId', (req, res) => {
+  const albumId = req.params.albumId;
+  deleteAlbumByAlbumId(albumId);
+  res.json({message: "Successfully deleted"});
+});
+
+app.get('/albums', (req, res) => {
+  const startsWith = req.query.startsWith;
+  if (startsWith) {
+    res.json(getFilteredAlbums(startsWith));
+  }
+});
+
+app.get('/artists/:artistId/songs', (req, res) => {
+  const artistId = req.params.artistId;
+  res.json(getSongsByArtistId(artistId));
+});
+
+app.get('/albums/:albumId/songs', (req, res) => {
+  const albumId = req.params.albumId;
+  res.json(getSongsByAlbumId(albumId));
+});
+
+app.get('/songs/:songId', (req, res) => {
+  const songId = req.params.songId;
+  res.json(getSongBySongId(songId));
+});
+
+app.post('/albumbs/:albumId/songs', (req, res) => {
+  const albumId = req.params.albumId;
+  const data = req.body;
+  res.json(addSongByAlbumId(albumId, data));
+});
+
+app.put('/songs/:songId', (req, res) => {
+  const songId = req.params.songId;
+  const data = req.body;
+  res.json(editSongBySongId(songId, data));
+});
+
+app.delete('/songs/:songId', (req, res) => {
+  const songId = req.params.songId;
+  deleteSongBySongId(songId);
+  res.json({message: "Successfully deleted"});
 });
 
 
